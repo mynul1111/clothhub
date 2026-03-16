5e47298d2eff839fe6c6c9a86e0aa513a9f270f9
mynul1111 <islamrhat12@gmail.com>
2026-03-16 18:54:12 +0600

    hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400'>
         {/* Hero Left Side */}
-        {/* <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
@@ -17,7 +17,7 @@ const Hero = () => {
                     <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                 </div>
             </div>
-        </div> */}
+        </div>
         {/* Hero Right Side */}
         <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
     </div>
